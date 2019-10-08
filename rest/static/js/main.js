1041f464b4b3fdf7fa37f7fb0632458f73d16cec
idrummer83 <vodaadminwater@gmail.com>
2019-10-08 23:36:31 +0300

    date

--- rest/static/js/main.js
+++ rest/static/js/main.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
     function compare(date){
         $('.get-table .badge').each(function(){
-            var q = $(this).data('table-date');
-            var a = q.split(',');
+            let q = $(this).data('table-date');
+            let a = q.split(',');
             a.pop();
 
             for (let i = 0; i < a.length; i++){
@@ -20,61 +20,86 @@ $(document).ready(function(){
     $('#datepicker').datepicker({
         dateFormat: "dd/mm/yy",
         onSelect: function () {
-                        var qwe = $(this).val();
-                        compare(qwe);
-                    }
+                    let qwe = $(this).val();
+                    compare(qwe);
+                    $('.control-date').text(qwe);
+                }
     });
 
-    // compare();
+    compare($('#datepicker').val());
+    $('.control-date').text($('#datepicker').val());
 
    $('.next-day').on('click', function () {
-       var date = $('#datepicker').datepicker('getDate');
-       date.setDate(date.getDate() +1)
+       let date = $('#datepicker').datepicker('getDate');
+       date.setDate(date.getDate() +1);
        $('#datepicker').datepicker('setDate', date);
-       var qqq = $('#datepicker').val();
+       let qqq = $('#datepicker').val();
 
        compare(qqq);
+       $('.control-date').text(qqq);
    });
 
    $('.prev-day').on('click', function () {
-       var date = $('#datepicker').datepicker('getDate');
+       let date = $('#datepicker').datepicker('getDate');
        date.setDate(date.getDate() -1);
        $('#datepicker').datepicker('setDate', date);
-       var qqq = $('#datepicker').val();
+       let qqq = $('#datepicker').val();
 
        compare(qqq);
+       $('.control-date').text(qqq);
    });
 
    $('.today-date').on('click', function () {
-       var qwe = $('#datepicker').val(moment().format('DD/MM/YYYY'));
+       let qwe = $('#datepicker').val(moment().format('DD/MM/YYYY'));
 
        compare(qwe.val());
+       $('.control-date').text(qwe.val());
    });
 
    /* new */
 
-    $('.get-table').on('click', function(){
-        var this_t = $(this);
-        $(this_t).toggleClass('badge-danger');
-        var t_id = $(this_t).attr('id');
-        var form = $('.form_template').clone()[0];
-        var act_form = $(form).attr('action');
+    $('.get-table').on('click', function(e){
 
-        if ($('.form_list .form_template').length ){
-
-            for (var i=0;i<$('.form_list .form_template').length;i++){
-                if ( $('.form_list .form_template')[i].getAttribute('id') == 'id-'+t_id ){
-                    $('.form_list .form_template')[i].remove();
-                    return false;
-                }
-            }
-            $('.form_list').append($(form).attr('action', act_form + t_id).attr('id', 'id-'+t_id));
-            form = null;
-
+        if ($(this).hasClass('badge-danger')) {
+            return false;
         } else {
-            $('.form_list').append($(form).attr('action', act_form + t_id).attr('id', 'id-'+t_id));
-            form = null;
+            // let q = $(this).children('.badge').data('table-date');
+            // let a = q.split(',');
+            // a.pop();
+            // console.log(a[1]);
+
+            let this_t = $(this);
+            $(this_t).toggleClass('badge-success');
+            let t_id = $(this_t).attr('id');
+            let form = $('.form_template').clone()[0];
+            let act_form = $(form).attr('action');
+
+            if ($('.form_list .form_template').length ){
+
+                for (let i=0;i<$('.form_list .form_template').length;i++){
+                    if ( $('.form_list .form_template')[i].getAttribute('id') == 'id-'+t_id ){
+                        $('.form_list .form_template')[i].remove();
+                        return false;
+                    }
+                }
+                $('.form_list').append(
+                    $(form).attr('action', act_form + t_id).attr('id', 'id-'+t_id)
+                );
+                let zzz = $('.control-date').text().toString();
+                $('.form_list #id-'+ t_id + ' #date').val(zzz);
+                $('.form_list #id-'+ t_id + ' #table').val(t_id);
+                form = null;
+
+            } else {
+                $('.form_list').append($(form).attr('action', act_form + t_id).attr('id', 'id-'+t_id));
+                let zzz = $('.control-date').text().toString();
+                $('.form_list #id-'+ t_id + ' #date').val(zzz);
+                $('.form_list #id-'+ t_id + ' #table').val(t_id);
+                    form = null;
+            }
         }
+
+
    });
 
 });
